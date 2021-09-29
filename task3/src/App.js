0027a243f28065456543fe0263e6ccfbaf5b25b3
bhatiashivam5 <bhatiashivam35@gmail.com>
2021-09-29 18:44:07 +0530

    first commit

---
+++ task3/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(()=>{
+    document.title = `you clicked ${count} times`;
+  });
+  return (
+    <div className="App">
+    you clicked {count} time<br />
+    <button onClick={()=>setCount(count+1)}>click me</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ task4/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import DataFetch from './DataFetch';
+
+function App() {
+  return (
+    <div className="App">
+     <DataFetch />
+    </div>
+  );
+}
+
+export default App;
